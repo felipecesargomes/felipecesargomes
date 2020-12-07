9cffb4f8e24912d7df2f797280ad64f02580ea34
felipecesargomes <www.felipecesargomes@gmail.com>
2020-12-06 22:50:14 -0300

    Atualização

--- PHP/conexaofabrica.php
+++
@@ -1,9 +0,0 @@
-<?php
-//teste
-  $servidor = "localhost";
-  $usuario = "root";
-  $senha = "";
-  $dbname = "teste";
-
-  $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-  ?>
--- POWERSHELL/apagar_arquivos.bat
+++
@@ -1,2 +0,0 @@
-del /q "C:\Users\wwwfe\Downloads\*"
-FOR /D %%p IN ("C:\Users\wwwfe\Downloads\*.*") DO rmdir "%%p" /s /q
